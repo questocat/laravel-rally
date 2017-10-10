69d49706f8d39c9b3ecd5afa926d115789f2d4ae
puzhengchao <zhengchaopu@gmail.com>
2017-10-10 23:55:45 +0800

    Update

--- src/Traits/Followable.php
+++ src/Traits/Followable.php
@@ -5,16 +5,4 @@ namespace Emanci\Rally\Traits;
 trait Followable
 {
     use CanFollow,CanBeFollowed;
-
-    /**
-     * Check if it is mutual follow.
-     *
-     *@param \Illuminate\Database\Eloquent\Model $followable
-     *
-     * @return bool
-     */
-    public function isMutualFollow(Model $followable)
-    {
-        return $this->isFollowing($followable) && $this->isFollowedBy($followable);
-    }
 }
